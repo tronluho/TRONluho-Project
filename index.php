2ec344a05b9d5fd243d7c82b86d0adcb6abb207d
Luho <luho@chrisluk.im>
2019-04-25 12:54:36 -0400

    added tron frontend

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="https://luho.moe/" />
+    <title>TRONluho (LUHO) Token</title>
+    <meta name="description" content="LUHO TRC-20 Token">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sticky-menu.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="css/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="css/favicon.png"/>
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Welcome</a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#whatwedo">What We Do</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>	<!-- .navbar-collapse -->
+        </div>		<!-- .container -->
+    </nav>
+    <!-- Welcome   -->
+    <section id="welcome" class="welcome-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+		    <h3>ANNOUNCEMENT!!!! ALL LUHO TOKENS HAVE SOLD! IF YOU DID NOT GET ANY LUHO YOU HAVE TO BUY OTC FROM SOMEONE ELSE!!!! <br>Giveaways using LUHO token start this FRIDAY, APRIL 26!!!!! <br>BE A LU, NOT A HO</h3>
+                    
+                    <h4>TRONluho LUHO TRC-20 Token!</h4>
+                    <p>This is currently a placeholder webpage for the LUHO token on the TRON blockchain.</p>
+					<p>Work is being done to bring value to all LUHO holders.</p>
+                    <br>
+                    <p>You may view the contract functions and call them from <a href="https://tronsmartcontract.space/#/interact/TYcZETwK7MwLdnkueMxyFkikfpKepHckwL">here</a></p>.<br><br>
+					<p>The contract address is <b>TYcZETwK7MwLdnkueMxyFkikfpKepHckwL</b>, you may add to TronLink to see your LUHO tokens if you have any. It is currently not supported on TronScan or TronWallet.</p><br><br>
+                    <a class="btn btn-primary page-scroll" href="#about">Click To Scroll Down!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About -->
+    <section id="about" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>About</h1>
+                    <p>There will only be 100,000 LUHO tokens that will ever be in existence. Half of this supply will be burned before the start of 2020. Holders of LUHO will be able to earn TRX through yours truly. I am still working on the specifics regarding how this will work.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- What we do Section -->
+    <section id="whatwedo" class="whatwedo-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>What I Do</h1>
+                    <p>I've been investing into cryptocurrencies since January 2018 and have started making videos about TRON since February of this year. My main mission is to educate people as much as possible on everything crypto related, in an effort to push the space closer to mass adoption.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 style="color:white;">Contact Section</h1>
+                    <p><a style="color:#66ff41;" href="https://t.me/tronluho">@tronluho</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+	<a id="back2Top" title="Back to top" href="#">&#10148;</a>
+	
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/sticky-menu.js"></script>
+
+</body>
+
+</html>
